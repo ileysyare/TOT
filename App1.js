d3a4ca4618de9f199164069ba5636454829ab04d
Mohamed Abdullahi Ali <ileysyare@gmail.com>
2022-10-19 11:51:49 +0300

    start

---
+++ App1.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text, Touchable, StyleSheet } from "react-native";
+
+const App=()=>{
+    return(
+        <View>
+
+            <Text style={styles.txt}>Hello APP</Text>
+            <Text style={styles.txt1}>Hello APP</Text>
+            <Text style={styles.txt1}>Hello APP</Text>
+        </View>
+    )
+};
+const styles=StyleSheet.create({
+    txt:{color:'white,',fontSize:20,backgroundColor:'blue'},
+    txt1:{color:'red',fontSize:20},
+
+});
+export default App;
